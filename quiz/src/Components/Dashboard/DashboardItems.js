726957fdfbfc3d05f861ecb3f9f1ce773a2804a8
DELO1 <ivandelai12@gmail.com>
2020-12-23 16:51:06 +0100

    merge + import

--- quiz/src/Components/Dashboard/DashboardItems.js
+++ quiz/src/Components/Dashboard/DashboardItems.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import "./Dashboard.css"
 
 export class DashboardItem extends Component {
     render() {
         return (
-            <Link className="dashboard__item" to={this.props.path}>
-            <div className="">
+            <Link  to={this.props.path}>
+            <div className="dashboard__item">
                 
-                <p>{this.props.name}</p>
+                <h1>{this.props.name}</h1>
                 
             </div>
             </Link>
