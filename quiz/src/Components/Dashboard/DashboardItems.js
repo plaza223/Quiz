14de04887616825b4da734ffcc2142efe7da0b84
DELO1 <ivandelai12@gmail.com>
2020-12-28 20:55:22 +0100

    dashboard-dashboardMain

--- quiz/src/Components/Dashboard/DashboardItems.js
+++ quiz/src/Components/Dashboard/DashboardItems.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import "./Dashboard.css"
+import "./Dashboard.css";
+
+
+
 
 export class DashboardItem extends Component {
+
+     
     render() {
+        
         return (
             <Link  to={this.props.path}  style={
             { textDecoration: 'none' }
             }>
                 
-            <div className="dashboard__container">
-                 <div className="dashboard__container__item">
-                
-            
+           
+                 <div className="dashboard__container">
+
+                    <div className="dashboard__container__icon">
+                       <img src={this.props.icon}></img>
+                    </div>  
+
+                    <div className="dashboard__conatainer__h1">
+                        <h1>{this.props.name}</h1>
+                    </div>
                 
                  </div>
-            <h1>{this.props.name}</h1>
+           
             
-            </div> 
+          
             
             
                
